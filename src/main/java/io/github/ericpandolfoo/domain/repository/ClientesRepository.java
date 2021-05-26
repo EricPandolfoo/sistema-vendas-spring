2d0ffe790c717b9a9b014a740b6559213f47aec4
Eric Pandolfo <eric.pandolfo@gmail.com>
2021-05-26 14:19:55 -0300

    Utilizando entityManager

--- src/main/java/io/github/ericpandolfoo/domain/repository/ClientesRepository.java
+++ src/main/java/io/github/ericpandolfoo/domain/repository/ClientesRepository.java
@@ -2,62 +2,56 @@ package io.github.ericpandolfoo.domain.repository;
 
 import io.github.ericpandolfoo.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class ClientesRepository {
 
-    private static String SELECT_POR_NOME = "select * from cliente where nome = ?";
-    private static String DELETE = "delete from cliente where id = ?" ;
-    private static String UPDATE = "update cliente set nome = ? where id = ?";
-    private static String SELECT_ALL = "select * from cliente";
-    private static String INSERT = "insert into cliente (nome) values (?) ";
-
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private EntityManager entityManager;
 
+    @Transactional
     public Cliente salvar(Cliente cliente) {
-        jdbcTemplate.update(INSERT, cliente.getNome());
+        entityManager.persist(cliente);
         return cliente;
     }
 
+    @Transactional
     public void deletar(Cliente cliente) {
-        deletar(cliente.getId());
+        if (!entityManager.contains(cliente)) {
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
     }
 
+    @Transactional
     public void deletar(Integer id) {
-        jdbcTemplate.update(DELETE, id);
+        deletar(entityManager.find(Cliente.class, id));
     }
 
+    @Transactional
     public Cliente atualizar(Cliente cliente) {
-        jdbcTemplate.update(UPDATE, cliente.getNome(), cliente.getId());
+        entityManager.merge(cliente);
         return cliente;
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> buscarPorNome(String nome) {
-        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "),
-                new Object[]{"%" + nome + "%"},
-                obterClienteMapper());
+        String jpql = " select c from Cliente c where c.nome like :nome ";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
-    }
-
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                Integer id = resultSet.getInt("id");
-                String nome = resultSet.getString("nome");
-                return new Cliente(id, nome);
-            }
-        };
+        return entityManager
+                .createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
