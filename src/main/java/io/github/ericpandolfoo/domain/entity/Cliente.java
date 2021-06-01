40e79771d8ce5f66c647d14c00dfa39879ed84dc
Eric Pandolfo <eric.pandolfo@gmail.com>
2021-06-01 01:41:14 -0300

    Adionando configuracao DB MySql

--- src/main/java/io/github/ericpandolfoo/domain/entity/Cliente.java
+++ src/main/java/io/github/ericpandolfoo/domain/entity/Cliente.java
@@ -19,7 +19,7 @@ public class Cliente {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "nome", length = 100)
--- src/main/java/io/github/ericpandolfoo/domain/entity/Pedido.java
+++ src/main/java/io/github/ericpandolfoo/domain/entity/Pedido.java
@@ -21,7 +21,7 @@ public class Pedido {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
